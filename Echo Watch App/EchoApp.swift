923ae5662dbddb071c426958a2ea876d469001e1
Saba Abesadze <saba72@live.com>
2026-03-13 17:27:36 +0400

    Initial Commit

---
+++ Echo Watch App/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo Watch App
+//
+//  Created by Saba Abesadze on 13/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Echo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Echo/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo
+//
+//  Created by Saba Abesadze on 13/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct EchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
